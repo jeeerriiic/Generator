98a1664837b308395a582821c1548e2c8f174f5d
Jeric <jericsmir@gmail.com>
2024-03-06 14:44:18 -0800

    titlePage, updated Side Navbar code, was not working properly, cleaned up css

--- titlePage/title.js
+++ titlePage/title.js
@@ -1,15 +1,16 @@
+let navbarContainer = document.getElementById('mySidenav');
+let pageContent = document.getElementById('main');
 let openButton = document.getElementById('openBtn');
-
 let closeButton = document.getElementById('closeBtn');
 
 function openNav() {
-    document.getElementById('mySidenav').style.width = '250px';
-    document.getElementById('main').style.marginLeft = '250px';
+    navbarContainer.style.width = '250px';
+    pageContent.style.marginLeft = '250px';
 }
 
 function closeNav() {
-    document.getElementById('mySidenav').style.width = '0';
-    document.getElementById('main').syle.marginLeft = '0';
+    navbarContainer.style.width = '0px';
+    pageContent.style.marginLeft = '0px';
 }
 
 openButton.addEventListener('click', function(){
