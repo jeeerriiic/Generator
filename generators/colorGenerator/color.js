d03a5aeed1228c67d6dbd59b597b0f2dd10b1563
Jeric <jericsmir@gmail.com>
2024-03-08 16:44:38 -0800

    colorGenerator, working on html/css/js, titlePage, updated css

--- generators/colorGenerator/color.js
+++ generators/colorGenerator/color.js
@@ -15,6 +15,7 @@ function randomIntFromInterval(min, max){
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
+// Container A
 function randomColorA(){
     let color = '#';
     for (let i = 0; i < 6; i++){
@@ -28,7 +29,9 @@ function randomColorA(){
     containerA.style.width = '25%';
     containerA.style.borderRight = '2px solid #000';
 }
+// Container A
 
+// Container B
 function randomColorB(){
     let color = '#';
     for (let i = 0; i < 6; i++){
@@ -41,10 +44,42 @@ function randomColorB(){
     containerB.style.width = '25%';
     containerB.style.borderRight = '2px solid #000';
 }
+// Container B
+
+// Container C
+function randomColorC(){
+    let color = '#';
+    for (let i = 0; i < 6; i++){
+        let getRandomColorC = validCharacters.charAt(randomIntFromInterval(0,15));
+        color += getRandomColorC;
+    };
+
+    displayHexC.innerHTML = color;
+    containerC.style.backgroundColor = color;
+    containerC.style.width = '25%';
+    containerC.style.borderRight = '2px solid #000';
+}
+// Container C
+
+// Container D
+function randomColorD(){
+    let color = '#';
+    for (let i = 0; i < 6; i++){
+        let getRandomColorD = validCharacters.charAt(randomIntFromInterval(0,15));
+        color += getRandomColorD;
+    };
+
+    displayHexD.innerHTML = color;
+    containerD.style.backgroundColor = color;
+    containerD.style.width = '25%';
+}
+// Container D
 
 randomColorA();
 randomColorB();
+randomColorC();
+randomColorD();
 
 button.addEventListener('click', function(){
-    randomColorA(), randomColorB();
+    randomColorA(), randomColorB(), randomColorC(), randomColorD();
 }, false);
