8766684db4e4a3650f71c815b4d31061e97175b7
Jeric <jericsmir@gmail.com>
2024-03-13 18:14:17 -0700

    usernameGenerator, js working

--- generators/usernameGenerator/username.js
+++ generators/usernameGenerator/username.js
@@ -4,6 +4,10 @@ let userButton = document.getElementById('usernameBtn');
 const userCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 const userLength = 12;
 
+function randomIntFromInterval(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
 function randomUsername() {
     let username = '';
     for(let i = 0; i < 12; i++) {
